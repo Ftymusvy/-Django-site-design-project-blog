558dc360b3ce16a9f302c19edd12584ab1ae4579
fatemeh <fatememosavinjd98@gmail.com>
2024-04-30 01:12:24 +0330

    error detection

--- pages/urls.py
+++ pages/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('' , views.index , name='index')#www.ryan-blog.com
+    path('' , views.index , name='index'), #www.ryan-blog.com\
+    path('about' , views.about , name='about')#www.ryan-blog.com\about
 ]
