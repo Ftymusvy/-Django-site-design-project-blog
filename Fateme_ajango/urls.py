bb705981231b3a9afea625afe1e9092125c3d47d
fatemeh <fatememosavinjd98@gmail.com>
2024-05-05 00:59:14 +0330

    tag errors

--- Fateme_ajango/urls.py
+++ Fateme_ajango/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path , include
 
+
 urlpatterns = [
     path('' , include('pages.urls')) ,#www.ryan-blog.com
     path('admin/', admin.site.urls),
--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import sys
+
 
 def index(request):
     return render(request, 'pages/index.html')
