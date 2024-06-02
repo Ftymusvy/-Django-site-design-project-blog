f675444eee7d9b992553bf8dfe8668dac52eb82d
fatemeh <fatememosavinjd98@gmail.com>
2024-06-03 00:54:55 +0330

    ctreate POSTS

--- Fateme_ajango/settings.py
+++ Fateme_ajango/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'posts.apps.PostsConfig',
     'pages.apps.PagesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
