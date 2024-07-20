81c45e4c0928d4d0f1a51d91fa11cf1faa809b9b
fatemeh <fatememosavinjd98@gmail.com>
2024-07-20 17:43:28 +0330

    log in and register (sabt name)

--- accounts/views.py
+++ accounts/views.py
@@ -1,15 +1,47 @@
 from django.shortcuts import render
-from django.contrib import messages
+from django.contrib import messages , auth
 from django.shortcuts import redirect
-
+from django.contrib.auth.models import User
+from bloggers.models import Blogger
 
 def register(request):
 
  if request.method == 'POST':
-    #register user
-    print('yes it is POST method')
-    messages.error(request, 'this is atest for error message.')
-    return redirect('register')  
+    #get form variable 
+
+
+    name = request.POST['name']
+    username = request.POST['username']
+    email = request.POST['email']
+    password = request.POST['password']
+    password2 = request.POST['password2']
+
+    #check if password match
+    if password == password2:
+       # check username
+       if User.objects.filter(username=username).exists():
+          messages.error(request , 'این نام کاربری در دسترس نیست')
+          return redirect('register')
+       else:
+        if User.objects.filter(email=email).exists():
+          messages.error(request ,'این ایمیل قبلا استفاده شده')
+          return redirect('register')
+          
+        else:
+           #its ok  
+           user = User.objects.create_user(username=username , password=password , email=email , first_name=name)
+           # create blogger
+           blooger = Blogger.objects.create(name= name , user=user , email=email )
+           # login after registration
+           auth.login(request , user)
+           messages.success(request , 'ثبت نام با موفقیت انجام شد')
+           
+           return redirect('index1')
+
+    else:
+       messages.error(request , 'رمز های عبور وارد شده مطابق نیستند')   
+       return redirect('register')
+
  else: 
     return render(request , 'accounts/register.html')
 
